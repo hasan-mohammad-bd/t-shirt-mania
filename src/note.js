8c0be590546082c578d9fd34a4dce33808b02c6d
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-04-01 15:38:32 +0600

    context api

--- src/note.js
+++ src/note.js
@@ -1,5 +1,9 @@
 /* 
 //1. make active route navigator: where the color or anything will be visitable.
 //2. conditional rendering in react (in cart)
-//
+//context: 4 steps 
+1. export RingContext = createContext("Dimond")
+2. cover the area with context 
+3. import the context where to use with context name.
+4. use the value inside the context.
 */
